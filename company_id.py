30b105b4037cb0ea60fedcd290fe508a709191a1
Maria Casado <maria.casado@lifeworks.com>
2022-09-23 13:32:10 +0100

    Make changes based on comments

--- company_id.py
+++ company_id.py
@@ -3,16 +3,13 @@ import json
 with open('assets/company.json') as f:
    company_data = json.load(f)
 
-
 with open('assets/user.json') as f:
    user_data = json.load(f)
-#    print(user_data)
 
 for user in user_data['empl_details']:
-    companyId = user['company_id']
+    company_id = user['company_id']
     for company in company_data:
-        if companyId == company['id']:
+        if company_id == company['id']:
             user['company'] = company
             user.pop("company_id")
             print(json.dumps(user))
-        
