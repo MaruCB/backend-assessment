887f5713aaa4f15826fd7ff821494bc0bda80184
Maria Casado <maria.casado@lifeworks.com>
2022-09-27 13:41:37 +0100

    no message

--- full_name.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('assets/user.json') as f:
-   data = json.load(f)
-
-for i in data:
-    full_name = f"{i['forename']} {i['surname']}"
-    i["full_name"] = full_name
-    print(json.dumps(i))
