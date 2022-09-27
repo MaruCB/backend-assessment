d2d94c5bc83b8f81b124995d8d5fa4380effb409
Maria Casado <maria.casado@lifeworks.com>
2022-09-27 13:37:32 +0100

    Completed classes, and change full_name.py to main.py to reflect all tasks merged in one

--- models.py
+++ models.py
@@ -1,15 +1,25 @@
+from typing import Dict
+
+
 class Company:
     id: int
     name: str
-    headquaters: str
+    headquarters: str
     industry: str
 
-    def __init__(self, id: int, name: str, headquaters: str, industry: str):
+    def __init__(self, id: int, name: str, headquarters: str, industry: str):
         self.id = id
         self.name = name
-        self.headquaters =  headquaters
+        self.headquarters =  headquarters
         self.industry = industry
 
+    def to_json(self):
+        return {
+            'id': self.id ,
+            'name': self.name ,
+            'headquarters': self.headquarters ,
+            'industry': self.industry
+        }
 
 class User:
     forename: str
@@ -17,13 +27,22 @@ class User:
     full_name: str
     date_of_birth: str
     location: str
-    company: Company # Is it?
+    company: Company
 
-    def __init__(self, forename: str, surname: str, full_name: str, date_of_birth: str, location: str):
+    def __init__(self, forename: str, surname: str, date_of_birth: str, location: str, company: Company):
         self.forename = forename
         self.surname = surname
-        self.full_name = full_name
+        self.full_name = f"{forename} {surname}"
         self.date_of_birth = date_of_birth
         self.location = location
+        self.company = company
 
-
+    def to_json(self):
+        return {
+            'forename': self.forename ,
+            'surname': self.surname ,
+            'full_name': self.full_name ,
+            'date_of_birth': self.date_of_birth ,
+            'location': self.location ,
+            'company': self.company.to_json()
+        }
